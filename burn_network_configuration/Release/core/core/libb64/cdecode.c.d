f6fa3428296d4128128b6ef72dfd10b960e3f327
Eric Mintz <16582299+emintz@users.noreply.github.com>
2025-01-21 12:46:09 -0500

    Delete burn_network_configuration/Release directory
    Stop tracking compiler output

--- burn_network_configuration/Release/core/core/libb64/cdecode.c.d
+++
@@ -1,5 +0,0 @@
-core/core/libb64/cdecode.c.o: \
- /home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cdecode.c \
- /home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cdecode.h
-
-/home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cdecode.h:
--- burn_network_configuration/Release/core/core/libb64/cencode.c.d
+++
@@ -1,5 +0,0 @@
-core/core/libb64/cencode.c.o: \
- /home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cencode.c \
- /home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cencode.h
-
-/home/eric/Sloeber/arduinoPlugin/packages/esp32/hardware/esp32/2.0.5/cores/esp32/libb64/cencode.h:
